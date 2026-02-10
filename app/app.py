f0ee82801b6872fb4d786f08f4c625bd557dfe7c
ChemaKing13 <jm2000ho@gmail.com>
2026-02-09 21:52:44 -0600

    feat: post schema set

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
 
-text_post = {
+text_posts = {
     1: {"title": "New Post", "content": "Cool test post"},
     2: {"title": "Learning FastAPI", "content": "FastAPI is fast and easy to use"},
     3: {"title": "Python Tips", "content": "Use list comprehensions to write cleaner code"},
@@ -18,12 +19,21 @@ text_post = {
 
 
 @app.get("/post")
-def get_all_posts(): 
-    return text_post
+def get_all_posts(limit: int = None):
+    if limit: 
+        return list(text_posts.values())[:limit] 
+    return text_posts
+
 
 
 @app.get("/post/{id}")
 def get_post(id: int): 
-    if id not in text_post:
+    if id not in text_posts:
         raise HTTPException(status_code=404, detail="Post not found")
-    return text_post.get(id)
+    return text_posts.get(id)
+
+@app.post("/post")
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
