6b8863e9f28cead63916082bd7d7c76aaab395c5
ChemaKing13 <jm2000ho@gmail.com>
2026-02-12 18:08:14 -0600

    retrieve posts endpoint

--- app/db.py
+++ app/db.py
@@ -4,13 +4,16 @@ import uuid
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import Declarative_Base, relationship
-import datetime
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
+class Base(DeclarativeBase): 
+    pass
 
-class Post(Declarative_Base): 
+
+class Post(Base): 
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -27,7 +30,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables(): 
     async with engine.begin() as conn: 
-        await conn.run_sync(Declarative_Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]: 
     async with async_session_maker() as session: 
